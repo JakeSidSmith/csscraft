49d34518da8dc44ce386b596a9a4a737e81a0412
Jake 'Sid' Smith <jake@dabapps.com>
2018-05-15 23:05:14 +0100

    Textures

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Viewport from './viewport';
 import Block from './block';
 
-const BLOCKS_PER_AXIS = 10;
+const BLOCKS_PER_AXIS = 8;
 
 const blocks = Array(BLOCKS_PER_AXIS).fill(null).map((valueZ, z) => {
   return Array(BLOCKS_PER_AXIS).fill(null).map((valueX, x) => {
