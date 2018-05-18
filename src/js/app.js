4906a88debf40e7339068a29b93fb89e12cd8edf
Jake 'Sid' Smith <jake@dabapps.com>
2018-05-18 10:47:48 +0100

    Store bocks as a set

--- src/js/app.js
+++ src/js/app.js
@@ -19,10 +19,36 @@ import Block from './block';
 //   });
 // });
 
-const App = () => (
-  <Viewport>
-    <Block x={0} y={0} z={0} />
-  </Viewport>
-);
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      blocks: new Set(['0x0x0'])
+    };
+  }
+  render () {
+    const {blocks} = this.state;
+
+    return (
+      <Viewport>
+        {
+          [...blocks].map((key) => {
+            const [x, y, z] = key.split('x');
+
+            return (
+              <Block
+                key={key}
+                x={x}
+                y={y}
+                z={z}
+              />
+            );
+          })
+        }
+      </Viewport>
+    );
+  }
+}
 
 export default App;
