4580f26d271cd3b44fccb2a1208311265e27a0f3
Jake 'Sid' Smith <jake@dabapps.com>
2018-05-18 09:38:00 +0100

    Rotate camera

--- src/js/app.js
+++ src/js/app.js
@@ -2,26 +2,26 @@ import React from 'react';
 import Viewport from './viewport';
 import Block from './block';
 
-const BLOCKS_PER_AXIS = 8;
+// const BLOCKS_PER_AXIS = 8;
 
-const blocks = Array(BLOCKS_PER_AXIS).fill(null).map((valueZ, z) => {
-  return Array(BLOCKS_PER_AXIS).fill(null).map((valueX, x) => {
-    const y = Math.floor(Math.random() * 2);
+// const blocks = Array(BLOCKS_PER_AXIS).fill(null).map((valueZ, z) => {
+//   return Array(BLOCKS_PER_AXIS).fill(null).map((valueX, x) => {
+//     const y = Math.floor(Math.random() * 2);
 
-    return (
-      <Block
-        key={`x=${x}y=${y}z=${z}`}
-        x={x - BLOCKS_PER_AXIS / 2}
-        y={y}
-        z={z - BLOCKS_PER_AXIS / 2}
-      />
-    );
-  });
-});
+//     return (
+//       <Block
+//         key={`x=${x}y=${y}z=${z}`}
+//         x={x - BLOCKS_PER_AXIS / 2}
+//         y={y}
+//         z={z - BLOCKS_PER_AXIS / 2}
+//       />
+//     );
+//   });
+// });
 
 const App = () => (
   <Viewport>
-    {blocks}
+    <Block x={0} y={0} z={0} />
   </Viewport>
 );
 
--- src/js/viewport.js
+++ src/js/viewport.js
@@ -1,13 +1,72 @@
 import React from 'react';
 
-const Viewport = ({children}) => (
-  <div className="viewport">
-    <div className="camera position">
-      <div className="camera rotation">
-        {children}
+class Viewport extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.lastMouse = null;
+
+    this.state = {
+      x: 45,
+      y: -45
+    };
+  }
+
+  pivot (x, y) {
+    return `rotateX(${y}deg) rotateY(${x}deg)`;
+  }
+
+  onMouseDown = (event) => {
+    const {clientX, clientY} = event;
+
+    this.lastMouse = {
+      clientX,
+      clientY
+    };
+
+    window.addEventListener('mousemove', this.onMouseMove);
+    window.addEventListener('mouseup', this.onMouseUp);
+  }
+
+  onMouseMove = (event) => {
+    event.preventDefault();
+
+    const {clientX, clientY} = event;
+
+    if (this.lastMouse) {
+      const {x, y} = this.state;
+
+      this.setState({
+        x: (x + clientX - this.lastMouse.clientX) % 360,
+        y: Math.max(Math.min(y + this.lastMouse.clientY - clientY, 90), -90)
+      });
+    }
+
+    this.lastMouse = {
+      clientX,
+      clientY
+    };
+  }
+
+  onMouseUp = () => {
+    this.lastMouse = null;
+
+    window.removeEventListener('mousemove', this.onMouseMove);
+    window.removeEventListener('mouseup', this.onMouseUp);
+  }
+
+  render () {
+    const { children } = this.props;
+    const { x, y } = this.state;
+
+    return (
+      <div className="viewport" onMouseDown={this.onMouseDown}>
+        <div className="camera" style={{transform: this.pivot(x, y)}}>
+          {children}
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Viewport;
