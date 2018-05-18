1f0063e264242d17094102c90e59673753d62d6e
Jake 'Sid' Smith <jake@dabapps.com>
2018-05-18 20:58:21 +0100

    Hard code sides

--- src/js/block.js
+++ src/js/block.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const faces = ['front', 'back', 'left', 'right', 'top', 'bottom']
-  .map((value) => (
-    <div key={value} className={`face ${value}`} />
-  ));
-
 function translate3d (x, y, z) {
   return `translate3d(${x * 64}px, ${y * 64}px, ${z * 64}px)`;
 }
 
 const Block = ({x, y, z}) => (
   <div className="block" style={{transform: translate3d(x, y, z)}}>
-    {faces}
+    <div className="face front" />
+    <div className="face back" />
+    <div className="face left" />
+    <div className="face right" />
+    <div className="face top" />
+    <div className="face bottom" />
   </div>
 );
 
