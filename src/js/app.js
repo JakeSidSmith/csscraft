f02c80dd13b01929e8ee326958edb7709c7a53b8
Jake 'Sid' Smith <jake@dabapps.com>
2018-05-18 10:48:02 +0100

    Remove commented out code

--- src/js/app.js
+++ src/js/app.js
@@ -2,23 +2,6 @@ import React from 'react';
 import Viewport from './viewport';
 import Block from './block';
 
-// const BLOCKS_PER_AXIS = 8;
-
-// const blocks = Array(BLOCKS_PER_AXIS).fill(null).map((valueZ, z) => {
-//   return Array(BLOCKS_PER_AXIS).fill(null).map((valueX, x) => {
-//     const y = Math.floor(Math.random() * 2);
-
-//     return (
-//       <Block
-//         key={`x=${x}y=${y}z=${z}`}
-//         x={x - BLOCKS_PER_AXIS / 2}
-//         y={y}
-//         z={z - BLOCKS_PER_AXIS / 2}
-//       />
-//     );
-//   });
-// });
-
 class App extends React.Component {
   constructor (props) {
     super(props);
